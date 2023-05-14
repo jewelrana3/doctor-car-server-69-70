520bfdf25ddd0ec0f4a51b9ccf77f30ea2987905
jewelrana3 <juyelrana7752@gmail.com>
2023-05-14 07:06:55 +0600

    cheakout

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
 require('dotenv').config()
-const app = express ();
 const port = process.env.PORT || 5000;
 
-// middle ware
-app.use(cors());
-app.use(express.json());
+// middleware
+app.use(cors())
+app.use(express.json())
 
 
 
@@ -26,12 +26,52 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const carCollection = client.db('carDoctors').collection('servies');
+    const cheakOut = client.db('carDoctors').collection('out');
+
+    app.get('/cars',async(req,res)=>{
+        const id = req.params.id;
+        const cursor = carCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    });
+
+    app.get('/cars/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            projection: { price:1, title: 1, service_id:1 },
+      
+          };
+        const result = await carCollection.findOne(query,options);
+        res.send(result)
+    });
+
+    app.get('/out',async(req,res)=>{
+      console.log(req.query.email)
+     let query = {}
+     if(req.query?.email){
+      query = {email:req.query.email}
+     }
+      const result = await cheakOut.find(query).toArray();
+      res.send(result) 
+    })
+
+    app.post('/cheakout',async(req,res)=>{
+      const cheakout = req.body;
+      console.log(cheakout);
+      const result = await cheakOut.insertOne(cheakout)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -39,9 +79,9 @@ run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
-    res.send('doctor is running')
+    res.send('doctor is runing')
 })
 
 app.listen(port,()=>{
-    console.log(`server is runing ,${port}`)
+    console.log(`server now is running,${port}`)
 })
